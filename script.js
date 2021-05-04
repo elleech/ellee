a0dc4f750fc23e1c0665f82624def01fe14d222e
elleech <elleech78@gmail.com>
2021-05-03 20:35:32 -0400

    add tooltips

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 $(function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    });
+
     var hash = window.location.hash;
     if (hash) {
         $('.panel').not(hash).hide();
